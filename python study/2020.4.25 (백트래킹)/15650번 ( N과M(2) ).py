96f9893e860f4e614a1731ebd589d9d9c55a86eb
오민영 <omy5123@naver.com>
2020-04-25 22:45:49 +0900

    폴더추가

---
+++ python study/2020.4.25 (백트래킹)/15650번 ( N과M(2) ).py
@@ -0,0 +1,81 @@
+"""
+문제
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+고른 수열은 오름차순이어야 한다.
+입력
+첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+
+출력
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+
+예제 입력 1
+3 1
+예제 출력 1
+1
+2
+3
+예제 입력 2
+4 2
+예제 출력 2
+1 2
+1 3
+1 4
+2 3
+2 4
+3 4
+예제 입력 3
+4 4
+예제 출력 3
+1 2 3 4
+"""
+from itertools import combinations
+n, m = map(int, input().split())
+
+a = list(map(str, range(1,n+1)))
+a = list(combinations(a, m))
+
+
+print('\n'.join(list(map(' '.join, a))))
+
+
+"""N, M = map(int, input().split())
+
+num_list = [i + 1 for i in range(N)]
+check_list = [False] * N
+
+arr = []
+
+def dfs(cnt):
+    # 주어진 개수만큼 채워지면 출력한다
+    if (cnt == M):
+        print(*arr)
+        return
+
+    for i in range(0, N):
+        if (check_list[i]):
+            continue
+
+        # i번째는 거쳐갈거라서 True로 바꾼다.
+        check_list[i] = True
+        arr.append(num_list[i])
+
+
+
+        # 현재의 i를 기준으로 가지치기 시작
+        dfs(cnt + 1)
+        # 이 부분은
+        #print(arr)
+        arr.pop()
+
+        # 여기서 print(arr)을 해보면 작동원리를 알 수 있다.
+
+        #print(check_list)
+        for j in range(i+1,N):
+            check_list[j] = False
+
+
+dfs(0)"""
