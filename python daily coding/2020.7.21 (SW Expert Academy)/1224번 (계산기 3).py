f17a1eb89b04830e4e162ad69023d1c37c39f40f
오민영 <omy5123@naver.com>
2020-07-21 14:42:45 +0900

    폴더추가

---
+++ python daily coding/2020.7.21 (SW Expert Academy)/1224번 (계산기 3).py
@@ -0,0 +1,42 @@
+import sys
+sys.stdin = open('input.txt')
+for t in range(1):
+    N = int(input())
+    num = []
+    stack = []
+    for i in input():
+        if i == '(':
+            stack.append(i)
+        elif i == '*':
+            if not stack or stack[-1] != '*':
+                stack.append(i)
+            else:
+                a = num.pop()
+                b = num.pop()
+                num.append(a*b)
+        elif i == '+':
+            if not stack or stack[-1] == '(':
+                stack.append(i)
+            else:
+                a = num.pop()
+                b = num.pop()
+                c = stack.pop()
+                if c == '*':
+                    num.append(a*b)
+                    stack.append(i)
+                else:
+                    num.append(a+b)
+                    stack.append(i)
+        elif i == ')':
+            c = stack.pop()
+            while c != '(':
+                a = num.pop()
+                b = num.pop()
+                if c == '+':
+                    num.append(a+b)
+                else:
+                    num.append(a*b)
+                c = stack.pop()
+        else:
+            num.append(int(i))
+    print('#{} {}'.format(t, num.pop()))
