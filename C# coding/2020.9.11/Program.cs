03fecb21cb9d70e3d6a68ea1eb4a831c19e70b1f
오민영 <omy5123@naver.com>
2020-09-12 19:27:41 +0900

    Update

---
+++ C# coding/2020.9.11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _1912__연속합_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] st = Console.ReadLine().Split();
+            int[] arr = new int[n+1];
+            arr[0] = 0;
+            for (int i = 1; i < n+1; i++)
+            {
+                arr[i] = int.Parse(st[i-1]);
+            }
+            int sum = Int32.MinValue;
+            int[] dp = new int[n + 1];
+            
+            for (int i = 1; i < n+1; i++)
+            {
+                dp[i] = Math.Max(arr[i], dp[i - 1] + arr[i]);
+                sum = Math.Max(sum, dp[i]);
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ C# coding/2020.9.11/_1463.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1463
+{
+    class _1463
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> dp = new List<int>() { 0,0,1,1,2,3 };
+            for (int i = 6; i < n+1; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    int a = dp[i / 3];
+                    int c = dp[i - 1];
+                    int min = Math.Min(a, c);
+                    if (i % 2 == 0)
+                    {
+                        int b = dp[i / 2];
+                        dp.Add(Math.Min(min, b)+1);
+                    }
+                    else
+                    {
+                        dp.Add(min+1);
+                    }
+                }
+                else if (i % 2 == 0)
+                {
+                    int min = (Math.Min(dp[i / 2], dp[i - 1]))+1;
+                    dp.Add(min);
+                }
+                else
+                {
+                    dp.Add(dp[i - 1]+1);
+                }
+            }
+            
+            Console.WriteLine(dp[n]);
+            
+        }
+    }
+}
