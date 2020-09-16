fb77232105d714d9d33ad4a120b7702bec10ea4c
오민영 <omy5123@naver.com>
2020-09-16 18:29:35 +0900

    Update

---
+++ C# coding/2020.9.16/_network.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _network
+{
+    class _network
+    {
+        static void dfs(int k, int[,] computers, int[] visit, int n)
+        {
+            visit[k] = 1;
+            for (int i = 0; i < n; i++)
+            {
+                if (visit[i] == 0 && computers[k, i] == 1)
+                {
+                    dfs(i, computers, visit, n);
+                }
+            }
+        }
+        static int solution(int n, int[,] computers)
+        {
+            int answer = 0;
+            int[] visit = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                if (visit[i] == 0)
+                {
+                    dfs(i, computers, visit, n);
+                    answer += 1;
+                }
+            }
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int[,] computers = { { 1, 1, 0 }, { 1, 1, 0 }, {0, 0, 1 }};
+            Console.WriteLine(solution(n, computers));
+        }
+    }
+
+}
---
+++ C# coding/2020.9.16/_numbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _두개_뽑아서_더하기
+{
+    class _numbers
+    {
+        static List<int> list = new List<int>();
+        static void dfs(int cnt, int[] numbers, int k, int sum)
+        {
+            if (cnt == 2)
+            {
+                if (list.Contains(sum) == false)
+                {
+                    list.Add(sum);
+                }
+            }
+            else
+            {
+                for (int i = k; i < numbers.Length; i++)
+                {
+                    sum += numbers[i];
+                    dfs(cnt + 1, numbers, i + 1, sum);
+                    sum -= numbers[i];
+                }
+            }
+        }
+        static void solution(int[] numbers)
+        {
+
+            dfs(0, numbers, 0, 0);
+            list.Sort();
+            foreach (var item in list)
+            {
+                Console.Write(item+" ");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 2, 1, 3, 4, 1 };
+            solution(numbers);
+        }
+    }
+}
