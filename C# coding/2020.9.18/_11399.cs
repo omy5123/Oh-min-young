2e1670c5c14d09b3d0e83a3a83168c193b8d5359
오민영 <omy5123@naver.com>
2020-09-18 20:54:31 +0900

    Update

---
+++ C# coding/2020.9.18/_11399.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _11399__ATM_
+{
+    class _11399
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] ar = Console.ReadLine().Split();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(ar[i]);
+            }
+            Array.Sort(arr);
+            int sum = arr[0];
+            for (int i = 1; i < n; i++)
+            {
+                arr[i] = arr[i]+arr[i - 1];
+                sum += arr[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ C# coding/2020.9.18/_joystick.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace _joystick
+{
+    class _joystick
+    {
+        static int solution(string name)
+        {
+            int answer = 0;
+            char[] chname = name.ToCharArray();
+            string str = "";
+            for (int i = 0; i < name.Length; i++)
+            {
+                str += 'A';
+            }
+            int idx = 0;
+            while (true)
+            {
+                int right = 1;
+                int left = 1;
+                if (chname[idx] != 'A')
+                {
+                    
+                    if ((int)chname[idx] - (int)'A' > 13)
+                        answer += 26 - ((int)chname[idx] - (int)'A');
+                    else
+                        answer += (int)chname[idx] - (int)'A';
+                    chname[idx] = 'A';
+                }
+                string ch = string.Join("", chname);
+                if (str == ch)
+                {
+                    break;
+                }
+                else
+                {
+                    for (int i = 1; i < chname.Length; i++)
+                    {
+                        if (chname[idx + i] == 'A')
+                        {
+                            right += 1;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        if(idx-i ==0)
+                        {
+                            if (chname[chname.Length + (idx - i)] == 'A')
+                            {
+                                left += 1;
+                            }
+                        }
+                        else
+                        {
+                            if (chname[idx - i] == 'A')
+                            {
+                                left += 1;
+                            }
+                        }
+                        
+
+                    }
+                    if (right > left)
+                    {
+                        answer += left;
+                        idx = chname.Length-left;
+                    }
+                    else
+                    {
+                        answer += right;
+                        idx += right;
+                    }
+                }
+
+            }
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            string name = "BBBAAAB";
+            int result = solution(name);
+            Console.WriteLine(result);
+        }
+    }
+}
