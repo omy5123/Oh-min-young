84cd99496099432c0d91bd90da8706ba6ad1b09c
오민영 <omy5123@naver.com>
2020-09-19 18:57:12 +0900

    Update

---
+++ C# coding/2020.9.19/_island.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _island
+{
+    class _island
+    {
+        static int[] Point = new int[101];
+        
+        static int solution(int n, int[,] costs)
+        {
+            int answer = 0;
+            List<Tuple<int, int, int>> arr = new List<Tuple<int, int, int>>();
+
+            for (int i = 0; i < costs.GetLength(0); i++)
+                arr.Add(new Tuple<int, int, int>(costs[i, 0], costs[i, 1], costs[i, 2]));
+
+            
+            arr = arr.OrderBy(x => x.Item3).ToList();
+
+            int[] visit = new int[n];
+            visit[0] = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                Point[i] = i;
+            }
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                int start = Find(arr[i].Item1);
+                int end = Find(arr[i].Item2);
+                int cost = arr[i].Item3;
+
+
+                if (start != end)
+                {
+                    Point[start] = end;
+
+                    answer += cost;
+                }
+            }
+
+        static int Find(int node)
+        {
+            if (node == Point[node]) return node;
+            else return Point[node] = Find(Point[node]);
+        }
+
+            return answer;
+        }
+        
+        static void Main(string[] args)
+        {
+            int n = 4;
+            int[,] costs = new int[,] { { 0, 1, 5 }, { 1, 2, 3 }, { 2, 3, 3 }, { 3, 1, 2 }, { 3, 0, 4 } };
+            Console.WriteLine(solution(n, costs));
+        }
+    }
+}
---
+++ C# coding/2020.9.19/prime number.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace _소수찾기
+{
+    class Program
+    {
+        static string numbers = "";
+        static List<string> result = new List<string>();
+
+        static void permute(String str,int l, int r)
+        {
+            if (l == r)
+            {
+                
+                result.Add(str);
+            }
+                
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = swap(str, l, i);
+                    permute(str, l + 1, r);
+                    str = swap(str, l, i);
+                }
+            }
+        }
+
+        static String swap(String a,int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+
+        static void solution(string numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result.Add(numbers[i].ToString());
+            }
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                permute(numbers, i, numbers.Length - 1);
+            }
+            
+
+            result = result.Distinct().ToList();
+            
+            List<int> arr = new List<int>();
+            for (int i = 0; i < result.Count; i++)
+            {
+                arr.Add(int.Parse(result[i]));
+            }
+            arr = arr.Distinct().ToList();
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            int count = 0;
+            
+            foreach (int item in arr)
+            {
+                bool flag = true;
+                if (item < 2)
+                {
+                    flag = false;
+                }
+                else if (item == 2)
+                {
+                    flag = true;
+                }
+                else
+                {
+                    for (int i = 2; i < item; i++)
+                    {
+                        if (item % i == 0)
+                        {
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+                if (flag)
+                {
+                    count += 1;
+                }
+            }
+            Console.WriteLine(count);
+
+        }
+        static void Main(string[] args)
+        {
+            numbers = "1230";
+            solution(numbers);
+        }
+    }
+}
