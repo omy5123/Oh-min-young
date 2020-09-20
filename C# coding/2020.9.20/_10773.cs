9a4f07d4f5eb7ba70c5ce4c629e98128d9415613
오민영 <omy5123@naver.com>
2020-09-20 19:55:44 +0900

    Update

---
+++ C# coding/2020.9.20/_10773.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10773
+{
+    class _10773
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            int k = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < k; i++)
+            {
+                int k_k = int.Parse(Console.ReadLine());
+                if (k_k == 0)
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(k_k);
+                }
+            }
+            Console.WriteLine(stack.Sum());
+        }
+    }
+}
---
+++ C# coding/2020.9.20/_1152.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1152
+{
+    class _1152
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            str = str.Trim();
+            if (str == "")
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                string[] s = str.Split();
+                Console.WriteLine(s.Length);
+            }
+            
+        }
+    }
+}
