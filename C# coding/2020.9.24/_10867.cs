f60b0cddc1a671f516dc56937f46918394620b7c
오민영 <omy5123@naver.com>
2020-09-24 18:33:49 +0900

    Update

---
+++ C# coding/2020.9.24/_10867.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _10867__중복_빼고_정렬하기_
+{
+    class _10867
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            string[] str = Console.ReadLine().Split();
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(str[i]);
+            }
+            arr = arr.Distinct().ToArray();
+            Array.Sort(arr);
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
---
+++ C# coding/2020.9.24/_5052.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace _5052__전화번호_목록_
+{
+    class _5052
+    {
+        static int n = 0;
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < t; i++)
+            {
+                n = int.Parse(Console.ReadLine());
+                string[] arr = new string[n];
+                for (int j = 0; j < n; j++)
+                {
+                    arr[j] = Console.ReadLine();
+                }
+                Array.Sort(arr);
+                bool check = true;
+                for (int k = 0; k < n-1; k++)
+                {
+                    if (arr[k+1].Contains(arr[k]))
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                {
+                    sb.AppendLine("YES");
+                }
+                else
+                {
+                    sb.AppendLine("NO");
+                }
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
