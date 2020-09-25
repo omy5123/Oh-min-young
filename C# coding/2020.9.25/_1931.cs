056e428c5e05c21279abc75208386df2bc20607a
오민영 <omy5123@naver.com>
2020-09-25 17:39:39 +0900

    Update

---
+++ C# coding/2020.9.25/_1931.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _1931__회의실배정_
+{
+    struct Cor
+    {
+        public int x;
+        public int y;
+    }
+    class _1931
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Cor[] arr = new Cor[n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] ar = Console.ReadLine().Split();
+                arr[i].x = int.Parse(ar[0]);
+                arr[i].y = int.Parse(ar[1]);
+            }
+            Array.Sort(arr, delegate (Cor u, Cor v)
+            {
+                if (u.x < v.x)
+                {
+                    return -1;
+                }
+                else if (u.x > v.x)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            });
+            Array.Sort(arr, delegate (Cor u, Cor v)
+            {
+                if (u.y < v.y)
+                {
+                    return -1;
+                }
+                else if (u.y > v.y)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            });
+            int last = 0;
+            int cnt = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int a = arr[i].x;
+                int b = arr[i].y;
+                if (a >= last)
+                {
+                    cnt += 1;
+                    last = b;
+                }
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
---
+++ C# coding/2020.9.25/_9935.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _9935__문자열_폭발_
+{
+    class _9935
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string check = Console.ReadLine();
+            StringBuilder sb = new StringBuilder(str.Length);
+
+            foreach (var item in str)
+            {
+                sb.Append(item.ToString());
+                if (sb.Length >= check.Length)
+                {
+                    bool flag = true;
+                    for (int i = 1; i < check.Length+1; i++)
+                    {
+                        if (sb[sb.Length-i] != check[check.Length-i])
+                        {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if (flag)
+                    {
+                        
+                        sb.Remove(sb.Length-check.Length,check.Length);
+                        
+                    }
+                }
+            }
+            if (sb.Length == 0)
+            {
+                Console.WriteLine("FRULA");
+            }
+            else
+            {
+                Console.WriteLine(sb);
+            }
+            
+            
+        }
+    }
+}
