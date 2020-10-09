d02244eec85ae4804927a38f45609fe6ff797a85
오민영 <omy5123@naver.com>
2020-10-09 22:33:31 +0900

    Update

---
+++ C# coding/2020.10.09/_10162.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _10162__가스레인지_
+{
+    class _10162
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            int[] time = new int[3];
+            
+            
+            time[0] += t / 300;
+            t %= 300;
+
+            time[1] += t / 60;
+            t %= 60;
+         
+            time[2] += t / 10;
+            t %= 10;
+          
+            
+            if (t != 0)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", time));
+            }
+        }
+    }
+}
---
+++ C# coding/2020.10.09/_1339.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _1339__단어_수학_
+{
+    class _1339
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var arr = new string[n];
+            var alphabets = new int[26];
+            for (int i = 0; i < n; i++)
+            {
+                string line = Console.ReadLine();
+                
+                for (int j = 0; j < line.Length; j++)
+                {
+                    Console.WriteLine(alphabets[line[j] - 'A']);
+                    alphabets[line[j] - 'A'] += (int)Math.Pow(10, line.Length - 1 - j);
+                }
+            }
+
+            Array.Sort(alphabets, (a, b) => a < b ? 1 : a == b ? 0 : -1);
+
+            int num = 9;
+            int result = 0;
+            for (int i = 0; i < alphabets.Length; i++)
+            {
+                if (alphabets[i] <= 0)
+                {
+                    break;
+                }
+                result += (alphabets[i] * num--);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
