d03ed55274ee80771f4f12c9e15c4a4e4468668b
오민영 <omy5123@naver.com>
2020-10-21 17:34:52 +0900

    Update

---
+++ C# coding/2020.10.21/_10800.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace _10800__컬러볼_
+{
+    class _10800
+    {
+        struct Cor
+        {
+            public int x;
+            public int y;
+            public int z;
+        }
+        static int[] solution(int n, int[,]arr)
+        {
+            Cor[] ball = new Cor[n];
+            for (int i = 0; i < n; i++)
+            {
+                ball[i].x = arr[i, 1];
+                ball[i].y = arr[i, 0];
+                ball[i].z = i;
+            }
+            Array.Sort(ball, delegate (Cor u, Cor v)
+            {
+                if(u.x<v.x)
+                {
+                    return -1;
+                }
+                else if(u.x>v.x)
+                {
+                    return 1;
+                }
+                else
+                {
+                    if(u.y<v.y)
+                    {
+                        return -1;
+                    }
+                    else if(u.y>v.y)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        if(u.z<v.z)
+                        {
+                            return -1;
+                        }
+                        else if(u.z>v.z)
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+                }
+            });
+
+            int[] color = new int[n];
+            int su = 0;
+            int j = 0;
+            int[] res = new int[n];
+            for (int i = 0; i < ball.Length; i++)
+            {
+                while(ball[j].x < ball[i].x)
+                {
+                    su += ball[j].x;
+                    color[ball[j].y - 1] += ball[j].x;
+                    j += 1;
+                }
+                res[ball[i].z] = su - color[ball[i].y - 1];
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] arr = new int[n, 2];
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine().Split();
+                for (int j = 0; j < 2; j++)
+                {
+                    arr[i, j] = int.Parse(line[j]);
+                }
+            }
+            int[]result = solution(n, arr);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+    }
+}
---
+++ C# coding/2020.10.21/_2170.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2170
+{
+    class Program
+    {
+        struct Cor
+        {
+            public int Item1;
+            public int Item2;
+        }
+        static int solution(int n, int[,] arr)
+        {
+            int answer = 0;
+            /*List<Tuple<int, int>> list = new List<Tuple<int, int>>();
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(new Tuple<int, int>(arr[i, 0], arr[i, 1]));
+            }
+            list = list.OrderBy(x => x).ToList();*/
+            Cor[] list = new Cor[n];
+            for (int i = 0; i < n; i++)
+            {
+                list[i].Item1 = arr[i, 0];
+                list[i].Item2 = arr[i, 1];
+            }
+            Array.Sort(list, delegate (Cor u, Cor v)
+            {
+                if(u.Item1 < v.Item1)
+                {
+                    return -1;
+                }
+                else if(u.Item1 > v.Item1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    if(u.Item2 < v.Item2)
+                    {
+                        return -1;
+                    }
+                    else if(u.Item2 > v.Item2)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+            });
+            int start = list[0].Item1;
+            int end = list[0].Item2;
+            answer += end - start;
+            for (int i = 1; i < n; i++)
+            {
+                if (end > list[i].Item1 && end < list[i].Item2)
+                {
+                    answer += list[i].Item2 - end;
+                    start = end;
+                    end = list[i].Item2;
+                }
+                else if (end <= list[i].Item1)
+                {
+                    answer += list[i].Item2 - list[i].Item1;
+                    start = list[i].Item1;
+                    end = list[i].Item2;
+                }
+            }
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] arr = new int[n, 2];
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine().Split();
+                arr[i, 0] = int.Parse(line[0]);
+                arr[i, 1] = int.Parse(line[1]);
+            }
+            Console.WriteLine(solution(n, arr));
+        }
+    }
+}
