9ff776184369986015ed831fbce11113d3799072
오민영 <omy5123@naver.com>
2020-10-26 22:52:33 +0900

    Update

---
+++ C# coding/2020.10.26/_1167.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1167__트리의_지름_
+{
+    class _1167
+    {
+        static int v;
+        static int inf = int.MaxValue;
+        static int[] dijkstra(int start, List<List<int[]>> list)
+        {
+            
+            int[] dp = new int[v + 1];
+            for (int j = 0; j < v+1; j++)
+            {
+                dp[j] = inf;
+            }
+            dp[start] = 0;
+            Queue<(int, int)> que = new Queue<(int, int)>();
+            que.Enqueue((0, start));
+            while (que.Count != 0)
+            {
+                (int a, int b) = que.Dequeue();
+                for (int k = 0; k < list[b].Count; k++)
+                {
+                    int n_n = list[b][k][0];
+                    int n_w = list[b][k][1];
+                    int wei = a + n_w;
+                    if (wei < dp[n_n])
+                    {
+                        dp[n_n] = wei;
+                        que.Enqueue((wei, n_n));
+                    }
+                }
+            }
+            
+            return dp;
+        }
+        static void Main(string[] args)
+        {
+            v = int.Parse(Console.ReadLine());
+            List<List<int[]>> list = new List<List<int[]>>();
+            for (int i = 0; i < v; i++)
+            {
+                string[] line = Console.ReadLine().Split();
+                int[] arr = new int[line.Length];
+                for (int j = 0; j < line.Length; j++)
+                {
+                    arr[j] = int.Parse(line[j]); 
+                }
+                int idx = 1;
+                int a = arr[0];
+                for (int j = 0; j < v+1; j++)
+                {
+                    list.Add(new List<int[]>());
+                }
+                while(arr[idx] != -1)
+                {
+                    int b = arr[idx];
+                    int c = arr[idx + 1];
+                    list[a].Add(new int[] { b, c });
+                    idx += 2;
+                }
+            }
+            int max = 0;
+            int[] result = dijkstra(1,list);
+            for (int i = 1; i < v + 1; i++)
+            {
+                if (result[i] == inf)
+                {
+                    continue;
+                }
+                else
+                {
+                    max = Math.Max(max, result[i]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+            int[] re = dijkstra(Array.IndexOf(result, max),list);
+            int answer = 0;
+            for (int i = 1; i < v + 1; i++)
+            {
+                if (result[i] == inf)
+                {
+                    continue;
+                }
+                else
+                {
+                    answer = Math.Max(answer, result[i]);
+                }
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ C# coding/2020.10.26/_2263.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2263__트리의_순회_
+{
+    class _2263
+    {
+        static int n;
+        static List<int> pre = new List<int>();
+        static List<int> pos = new List<int>();
+        static int[] inorder;
+        static int[] postorder;
+        static int root;
+        static StringBuilder sb = new StringBuilder();
+        static void divide(int in_start,int in_end,int post_start, int post_end)
+        {
+            if ((in_start > in_end) || (post_start > post_end))
+            {
+                return;
+            }
+            root = postorder[post_end]; //pos가 0부터 시작이라서
+
+            sb.Append(root + " ");
+            int p = pos[root];
+            int left = p - in_start;
+            divide(in_start, p - 1, post_start, post_start + left - 1);
+            divide(p + 1, in_end, post_start +left, post_end - 1);
+
+        }
+        static void solution(int n, int[] inorder, int[] postorder)
+        {
+            for (int i = 0; i < n + 1; i++)
+            {
+                pos.Add(0);
+            }
+
+
+            for (int i = 0; i < n; i++)
+            {
+                pos[inorder[i]] = i;
+            }
+
+            divide(0, n - 1, 0, n - 1);
+            Console.WriteLine(sb);
+        }
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            inorder = new int[n];
+            string[] line = Console.ReadLine().Split();
+            for (int i = 0; i < n; i++)
+            {
+                inorder[i] = int.Parse(line[i]);
+            }
+            postorder = new int[n];
+            string[] line_1 = Console.ReadLine().Split();
+            for (int i = 0; i < n; i++)
+            {
+                postorder[i] = int.Parse(line_1[i]);
+            }
+            solution(n, inorder, postorder);
+        }
+    }
+}
