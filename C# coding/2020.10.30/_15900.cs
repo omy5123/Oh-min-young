cbf07ae07ec844351cd14c3abb43611ce81f22d9
오민영 <omy5123@naver.com>
2020-10-30 20:36:59 +0900

    Update

---
+++ C# coding/2020.10.30/_15900.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace _15900__나무_탈출_
+{
+    class _15900
+    {
+        static long n;
+        static List<List<int>> list = new List<List<int>>();
+        static long depth = 0;
+        static void dfs(int start, long[,]arr, long[] visit,int hei)
+        {
+            visit[start] = 1;
+            bool isLeaf = true;
+            for (int i = 0; i < list[start].Count; i++)
+            {
+                int a = list[start][i];
+                if(visit[a] == 0)
+                {
+                    isLeaf = false;
+                    dfs(a, arr, visit,hei+1);
+                }
+            }
+            if(isLeaf)
+            {
+                depth += hei;
+            }
+        }
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            long[,] arr = new long[n+1,n+1];
+            for (int i = 0; i < n+1; i++)
+            {
+                list.Add(new List<int>());
+            }
+            for (int i = 0; i < n-1; i++)
+            {
+                string[] line = Console.ReadLine().Split();
+                int a = int.Parse(line[0]);
+                int b = int.Parse(line[1]);
+                list[a].Add(b);
+                list[b].Add(a);
+            }
+            long[] visit = new long[n + 1];
+            dfs(1, arr,visit,0);
+            
+            if(depth % 2 == 0)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine("Yes");
+            }
+
+        }
+    }
+}
---
+++ C# coding/2020.10.30/_2960.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace _2960__에라토스테네스의_체_
+{
+    class _2960
+    {
+        static void Main(string[] args)
+        {
+            string[] nk = Console.ReadLine().Split();
+            int n = int.Parse(nk[0]);
+            int k = int.Parse(nk[1]);
+
+            bool[] check = new bool[n + 1];
+            int cnt = 0;
+
+            for (int i = 2; i < n+1; i++)
+            {
+                for (int j = i; j < n+1;)
+                {
+                    if(check[j]==false)
+                    {
+                        check[j] = true;
+                        cnt += 1;
+                        if(cnt == k)
+                        {
+                            Console.WriteLine(j);
+                            return;
+                        }
+                    }
+                    j = j + i;
+                }
+            }
+        }
+    }
+}
