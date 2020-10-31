a161336c7caad1724131ac9907b1fd3240d5447c
오민영 <omy5123@naver.com>
2020-10-31 18:05:23 +0900

    Update

---
+++ C# coding/2020.10.31/_1094.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+
+namespace _1094
+{
+    class _1094
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            int cnt = 0;
+            while(x != 0)
+            {
+                if(x%2 == 1)
+                {
+                    cnt += 1;
+                }
+                x /= 2;
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
