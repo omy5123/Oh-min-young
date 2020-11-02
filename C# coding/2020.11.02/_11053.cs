a09870bfa45996cba95c07aeadca23e9eb533a58
오민영 <omy5123@naver.com>
2020-11-02 23:47:13 +0900

    Update

---
+++ C# coding/2020.11.02/_11053.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _11053__가장_긴_증가하는_부분수열_
+{
+    class _11053
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] st = Console.ReadLine().Split();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(st[i]);
+            }
+            int[] dp = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                dp[i] = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[j]<arr[i]&&dp[j]+1>dp[i])
+                    {
+                        dp[i] += 1;
+                    }
+                }
+            }
+            Console.WriteLine(dp.Max());
+        }
+    }
+}
---
+++ C# coding/2020.11.02/_1149.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _1149__RGB거리_
+{
+    class _1149
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] arr = new int[n, 3];
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine().Split();
+                for (int j = 0; j < 3; j++)
+                {
+                    arr[i, j] = int.Parse(line[j]);
+                }
+            }
+            for (int i = 1; i < n; i++)
+            {
+                arr[i, 0] += Math.Min(arr[i - 1, 1], arr[i - 1, 2]);
+                arr[i, 1] += Math.Min(arr[i - 1, 0], arr[i - 1, 2]);
+                arr[i, 2] += Math.Min(arr[i - 1, 0], arr[i - 1, 1]);
+            }
+            int min = int.MaxValue;
+            for (int i = 0; i < 3; i++)
+            {
+                if(min > arr[n-1,i])
+                {
+                    min = arr[n - 1, i];
+                }
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
