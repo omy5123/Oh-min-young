54806c212c8d39caf8292b252354d5addc297067
오민영 <omy5123@naver.com>
2020-11-04 18:19:48 +0900

    Update

---
+++ C# coding/2020.11.04/_1145.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _1145__적어도_대부분의_배수_
+{
+    class _1145
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split();
+            int[] arr = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                arr[i] = int.Parse(line[i]);
+            }
+            int min = arr.Min();
+            while(true)
+            {
+                int cnt = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    if(min % arr[i] == 0)
+                    {
+                        cnt += 1;
+                    }
+                }
+                if (cnt >=3)
+                {
+                    Console.WriteLine(min);
+                    break;
+                }
+                min += 1;
+            }
+        }
+    }
+}
---
+++ C# coding/2020.11.04/_2966.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+
+namespace _2966__찍기_
+{
+    class _2966
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string str = Console.ReadLine();
+
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    if(str[i] == 'A')
+                    {
+                        a += 1;
+                    }
+                }
+                else if (i % 3 == 1)
+                {
+                    if(str[i] == 'B')
+                    {
+                        a += 1;
+                    }
+                }
+                else
+                {
+                    if(str[i] == 'C')
+                    {
+                        a += 1;
+                    }
+                }
+
+                if (i % 4 == 0 || i % 4 == 2)
+                {
+                    if (str[i] == 'B')
+                    {
+                        b += 1;
+                    }
+                }
+                else if (i % 4 == 1)
+                {
+                    if (str[i] == 'A')
+                    {
+                        b += 1;
+                    }
+                }
+                else
+                {
+                    if (str[i] == 'C')
+                    {
+                        b += 1;
+                    }
+                }
+
+                if (i % 6 == 0 || i% 6 ==1)
+                {
+                    if (str[i] == 'C')
+                    {
+                        c += 1;
+                    }
+                }
+                else if (i % 6 == 2 || i% 6 == 3)
+                {
+                    if (str[i] == 'A')
+                    {
+                        c += 1;
+                    }
+                }
+                else
+                {
+                    if (str[i] == 'B')
+                    {
+                        c += 1;
+                    }
+                }
+            }
+            int[] max = { a, b, c };
+            int m = max.Max();
+            Console.WriteLine(m);
+
+            if(a == m)
+            {
+                Console.WriteLine("Adrian");
+            }
+            if(b == m)
+            {
+                Console.WriteLine("Bruno");
+            }
+            if (c == m)
+            {
+                Console.WriteLine("Goran");
+            }
+        }
+    }
+}
